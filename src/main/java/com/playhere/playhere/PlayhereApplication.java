ea4e7cca4ca0940cdb0af5a065371b62d7d8762e
CompleteYourGoal <59370268+chetalipolekar@users.noreply.github.com>
2021-09-19 15:37:36 +0530

    Added Spring boot initial project

---
+++ src/main/java/com/playhere/playhere/PlayhereApplication.java
@@ -0,0 +1,13 @@
+package com.playhere.playhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayhereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playhere/playhere/PlayhereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playhere.playhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayhereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
