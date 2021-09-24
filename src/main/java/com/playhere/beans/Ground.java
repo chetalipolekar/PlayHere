17d3234301bbf3b5baafc102512608a628d1273a
CompleteYourGoal <59370268+chetalipolekar@users.noreply.github.com>
2021-09-24 11:48:28 +0530

    commit

--- src/main/java/com/playhere/beans/Ground.java
+++ src/main/java/com/playhere/beans/Ground.java
@@ -24,6 +24,7 @@ public class Ground {
 	public Ground() {
 		super();
 	}
+	
 	public Ground(Long gid, String location, String groundType, String image, String sports) {
 		super();
 		this.gid = gid;
@@ -32,6 +33,55 @@ public class Ground {
 		this.image = image;
 		Sports = sports;
 	}
+	
+	public Long getGid() {
+		return gid;
+	}
+
+	public void setGid(Long gid) {
+		this.gid = gid;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getGroundType() {
+		return groundType;
+	}
+
+	public void setGroundType(String groundType) {
+		this.groundType = groundType;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getSports() {
+		return Sports;
+	}
+
+	public void setSports(String sports) {
+		Sports = sports;
+	}
+
+	public Users getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Users owner) {
+		this.owner = owner;
+	}
+
 	@Override
 	public String toString() {
 		return "Ground [gid=" + gid + ", location=" + location + ", groundType=" + groundType + ", image=" + image
--- src/main/java/com/playhere/config/SecurityConfiguration.java
+++ src/main/java/com/playhere/config/SecurityConfiguration.java
@@ -1,11 +1,13 @@
 package com.playhere.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -20,8 +22,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/login").hasAnyRole("Users", "Owner").antMatchers("/").permitAll().and()
+		http.authorizeRequests().antMatchers("/user").hasAnyRole("user", "owner").antMatchers("/").permitAll().and()
 				.formLogin();
 
+	}
+
+	@Bean
+	public static NoOpPasswordEncoder passwordEncoder() {
+		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
 	}
 }
